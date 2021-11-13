0a4ceb188f6a824fad428a8ef30d26c20b8daa36
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-13 13:12:53 -0700

    Add imperial units

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,6 +1,6 @@
 class OpenWeatherService < ApplicationService
   def self.get_weather(lat, lon)
-    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely,alerts&appid=#{ENV['open_weather_key']}")
+    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely,alerts&units=imperial&appid=#{ENV['open_weather_key']}")
     parse_json(response)
   end
 end
