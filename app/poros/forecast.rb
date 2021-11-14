b4c026b3e0befddef15946d4879a7f734986c202
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-13 18:33:04 -0700

    Add forecast

---
+++ app/poros/forecast.rb
@@ -0,0 +1,29 @@
+class Forecast
+  attr_reader :current_weather,
+              :daily_weather,
+              :hourly_weather
+
+  def initialize(data)
+    @current_weather = CurrentForecast.new(data[:current])
+    @daily_weather   = create_daily_forecasts(data[:daily])
+    @hourly_weather  = create_hourly_forecasts(data[:hourly])
+  end
+
+  def create_daily_forecasts(data)
+    i = 0
+    daily = []
+    5.times do |i|
+      daily << DailyForecast.new(data[i])
+    end
+    daily
+  end
+
+  def create_hourly_forecasts(data)
+    i = 0
+    hourly = []
+    8.times do |i|
+      hourly << HourlyForecast.new(data[i])
+    end
+    hourly
+  end
+end
