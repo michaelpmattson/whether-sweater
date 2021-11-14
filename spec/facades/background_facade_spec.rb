8d69fa5c632c6ef4b784cf8b30fbfdf6117a18bf
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-14 13:36:41 -0700

    Remove pry

--- spec/facades/background_facade_spec.rb
+++ spec/facades/background_facade_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe BackgroundFacade do
     it 'returns a background object' do
       location = 'denver,co'
       expect(BackgroundFacade.get_background(location)).to be_a(Background)
-      binding.pry
     end
   end
 end
