14a1ac0192eaef0874348437ec7619eb676e4b16
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-13 20:09:40 -0700

    Add ForecastController index, can get a forecast

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
+    geo      = MapquestFacade.get_geolocation(params[:location])
+    forecast = OpenWeatherFacade.get_forecast({lat: geo[:lat], lon: geo[:lng]})
+
+    render json: ForecastSerializer.format_forecast(forecast), status: :ok
   end
 end
