13c8b90afad9ba29ae1d86d6aed94668d06bf12d
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-13 20:10:31 -0700

    Update to hash

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,6 +1,6 @@
 class OpenWeatherService < ApplicationService
-  def self.get_weather(lat, lon)
-    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely,alerts&units=imperial&appid=#{ENV['open_weather_key']}")
+  def self.get_weather(geolocation)
+    response = Faraday.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{geolocation[:lat]}&lon=#{geolocation[:lon]}&exclude=minutely,alerts&units=imperial&appid=#{ENV['open_weather_key']}")
     parse_json(response)
   end
 end
