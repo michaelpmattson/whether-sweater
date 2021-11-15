aac4a392c0fcc9c27f8481b213f695714588db17
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-14 19:19:20 -0700

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true, on: :create
+
+  def self.generate_api_key
+    SecureRandom.urlsafe_base64
+  end
 end
