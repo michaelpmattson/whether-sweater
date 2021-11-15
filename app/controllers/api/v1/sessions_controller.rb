7a545051122ce79ee3d1ab7eb43716aee30e4798
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-15 14:44:20 -0700

    Test create session happy path

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UsersSerializer.new(user), status: :ok
+    else
+    end
   end
 end
--- spec/requests/api/v1/sessions/create_spec.rb
+++ spec/requests/api/v1/sessions/create_spec.rb
@@ -9,22 +9,26 @@ RSpec.describe 'the post sessions endpoint' do
     post('/api/v1/sessions', params: { email: 'bigboy@email.com', password: 'potato' })
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body).to                     have_key(:data)
+    expect(body[:data]).to              have_key(:type)
+    expect(body[:data]).to              have_key(:id)
+    expect(body[:data]).to              have_key(:attributes)
+    expect(body[:data][:attributes]).to have_key(:email)
+    expect(body[:data][:attributes]).to have_key(:api_key)
+
+    expect(body[:data]).to                        be_a(Hash)
+    expect(body[:data][:type]).to                 eq('users')
+    expect(body[:data][:id]).to                   eq(@user.id.to_s)
+    expect(body[:data][:attributes]).to           be_a(Hash)
+    expect(body[:data][:attributes][:email]).to   eq(@user.email)
+    expect(body[:data][:attributes][:api_key]).to eq(@user.api_key)
   end
 end
 
-# Your api should expose this endpoint:
-#
-# Request:
-#
-# POST /api/v1/sessions
-# Content-Type: application/json
-# Accept: application/json
-#
-# {
-#   "email": "whatever@example.com",
-#   "password": "password"
-# }
-# Response:
 #
 # status: 200
 # body:
