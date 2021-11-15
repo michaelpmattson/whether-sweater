f5abfe6a7b1cc0297834f3724afbee782b61a5cf
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-15 09:20:00 -0700

    Make route and index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :activities,  only: :index
       resources :backgrounds, only: :index
       resources :forecast,    only: :index
       resources :users,       only: :create
