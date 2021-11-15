95c83aab252fb36acb0a26a465622b4339c0fbc6
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-15 10:46:53 -0700

    Refactor to use map

--- app/serializers/activities_serializer.rb
+++ app/serializers/activities_serializer.rb
@@ -10,22 +10,15 @@ class ActivitiesSerializer
             summary: forecast.current_weather.conditions,
             temperature: "#{forecast.current_temperature} F"
           },
-          activities: [
+          activities: activities.map do |type, activity|
             {
-              activities[:relaxation].name => {
-                type: activities[:relaxation].type,
-                participants: activities[:relaxation].participants,
-                price: activities[:relaxation].price
-              }
-            },
-            {
-              activities[:other].name => {
-                type: activities[:other].type,
-                participants: activities[:other].participants,
-                price: activities[:other].price
+              activity.name => {
+                type: activity.type,
+                participants: activity.participants,
+                price: activity.price
               }
             }
-          ]
+          end
         }
       }
     }
